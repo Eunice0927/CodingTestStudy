102541774d2e6df3b7739822109bba2e97a52baf
Eunice JEONG <jes.e.0927@gmail.com>
2023-10-12 00:26:20 +0900

    ♻️ Refactor: Change code to function / Change annotation format
    Command Line Tool 특성 상 코드가 main에서만 작동하는데 문제 풀이에 필요한 코드들은 main에 작성되어야 한다.
    하나의 repository를 연동하므로 여러개의 main파일을 가질 수 없고, 매 번 오류가 발생하는 행을 주석처리 할 수 없으므로
    전체 코드를 함수 안에 포함시켰다.
    Xcode에서는 해당 함수를 main파일에서 호출하여 실행하고, 백준 제출시에는 코드 제일 하단에서 함수를 호출한다.
    
    상단의 문제 설명 부분의 양식을 통일감있게 변경하였다.

--- CodingTestStudy/BaekJoon/11382.swift
+++ CodingTestStudy/BaekJoon/11382.swift
@@ -5,27 +5,31 @@
 //  Created by Eunsu JEONG on 2023/10/11.
 //
 
+/* 문제 */
 // 꼬마 정민이는 이제 A + B 정도는 쉽게 계산할 수 있다. 이제 A + B + C를 계산할 차례이다!
+/* 입력 */
 // 첫 번째 줄에 A, B, C (1 ≤ A, B, C ≤ 1012)이 공백을 사이에 두고 주어진다.
+/* 출력 */
 // A+B+C의 값을 출력한다.
+/* 예제 */
 // 77 77 7777 -> 7931
 
 import Foundation
 
-print("입력:", terminator: " ")
-private var answer: Int = 0
-private var number: Int = 0
+func quiz11382() {
+    print("입력:", terminator: " ")
+    var answer: Int = 0
 
-private let inputValue: String = readLine() ?? ""
-private let array = inputValue.components(separatedBy: " ")
+    let inputValue: String = readLine() ?? ""
+    let array = inputValue.components(separatedBy: " ")
 
-for i in 0..<array.count {
-    if let number = Int(array[i]) {
-        answer = answer + number
-    }else {
-        print("입력 값이 잘못되었습니다. 숫자만 입력 해 주세요.")
-        answer = answer + 0
+    for i in 0..<array.count {
+        if let number = Int(array[i]) {
+            answer = answer + number
+        }else {
+            print("입력 값이 잘못되었습니다. 숫자만 입력 해 주세요.")
+            answer = answer + 0
+        }
     }
+    print(answer)
 }
-
-print(answer)
