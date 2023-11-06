2763df7f002f028c4037c6e8699d43f795334194
Eunice <jes.e.0927@gmail.com>
2023-11-06 10:04:07 +0900

    🎉 Init: Start new quiz(BaekJoon 11720)
    ⚠️ Fail: 컴파일 에러
    에러 메세지: cannot convert value of type 'String' to expected argument type 'String.Element' (aka 'Character')

---
+++ CodingTestStudy/BaekJoon/11720.swift
@@ -0,0 +1,38 @@
+//
+//  11720.swift
+//  CodingTestStudy
+//
+//  Created by Eunsu JEONG on 11/6/23.
+//
+/* 문제 */
+// N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+/* 입력 */
+// 첫째 줄에 숫자의 개수 N (1 ≤ N ≤ 100)이 주어진다. 둘째 줄에 숫자 N개가 공백없이 주어진다.
+/* 출력 */
+// 입력으로 주어진 숫자 N개의 합을 출력한다.
+/* 예제 */
+/*
+ 1
+ 1
+ -> 1
+ 1
+ 54321
+ -> 15
+ 11
+ 10987654321
+ -> 46
+ */
+
+import Foundation
+
+func quiz11720() {
+    let numberQty: Int = Int(readLine()!)!
+    let numberArray = readLine()!.split(separator: "")
+    var sum: Int = 0
+    
+    for i in 0..<numberQty {
+        let number: Int = Int(String(numberArray[i]))!
+        sum += number
+    }
+    print(sum)
+}
