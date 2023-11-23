c14c6c68b6f4eece397b3a5e67b4a017df88c62e
Eunice <jes.e.0927@gmail.com>
2023-11-23 10:27:23 +0900

    🚀 Success: 정답입니다!

--- CodingTestStudy/Programmers/lv0_181946.swift
+++ CodingTestStudy/Programmers/lv0_181946.swift
@@ -15,3 +15,10 @@
 // applepen
 
 import Foundation
+
+func quiz181946() {
+    let inp = readLine()!.components(separatedBy: [" "]).map { $0 }
+    let (s1, s2) = (inp[0], inp[1])
+    
+    print(s1 + s2)
+}
