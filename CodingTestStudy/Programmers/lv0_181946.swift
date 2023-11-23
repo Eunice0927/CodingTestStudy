ff35f1b1d68ed4a88f3f10bb8c6c4dc573457790
Eunice <jes.e.0927@gmail.com>
2023-11-23 10:24:55 +0900

    🎉 Init: Start new quiz(Programmers Lv.0 181946 문자열 붙여서 출력하기)

---
+++ CodingTestStudy/Programmers/lv0_181946.swift
@@ -0,0 +1,17 @@
+//
+//  lv0_181946.swift
+//  CodingTestStudy
+//
+//  Created by Eunsu JEONG on 11/23/23.
+//
+/* 제목 */
+// 문자열 붙여서 출력하기
+/* 문제 */
+// 두 개의 문자열 str1, str2가 공백으로 구분되어 입력으로 주어집니다.
+// 입출력 예와 같이 str1과 str2을 이어서 출력하는 코드를 작성해 보세요.
+/* 입력 */
+// apple pen
+/* 출력 */
+// applepen
+
+import Foundation
