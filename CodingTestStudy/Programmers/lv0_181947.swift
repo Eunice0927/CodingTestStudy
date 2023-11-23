b468d6ed34c9a92329b633af35197ead970c0488
Eunice <jes.e.0927@gmail.com>
2023-11-23 10:22:49 +0900

    🚀 Success: 정답입니다!

--- CodingTestStudy/Programmers/lv0_181947.swift
+++ CodingTestStudy/Programmers/lv0_181947.swift
@@ -14,3 +14,10 @@
 // 4 + 5 = 9
 
 import Foundation
+
+func quiz181947() {
+    let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+    let (a, b) = (n[0], n[1])
+
+    print("\(a) + \(b) = \(a + b)")
+}
