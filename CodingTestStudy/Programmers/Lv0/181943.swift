55e0fbc53388c3ec0500a34e1feea2bfab4c4704
Eunice <jes.e.0927@gmail.com>
2023-11-24 13:21:33 +0900

    🎉 Init: Start new quiz(Programmers Lv.0 181943 문자열 겹쳐쓰기)

---
+++ CodingTestStudy/Programmers/Lv0/181943.swift
@@ -0,0 +1,22 @@
+//
+//  181943.swift
+//  CodingTestStudy
+//
+//  Created by Eunsu JEONG on 11/24/23.
+//
+/* 제목 */
+// 문자열 겹쳐쓰기
+/* 문제 */
+// 문자열 my_string, overwrite_string과 정수 s가 주어집니다.
+//문자열 my_string의 인덱스 s부터 overwrite_string의 길이만큼을
+// 문자열 overwrite_string으로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
+/* 입력 */
+/*
+ my_string = "He11oWor1d"
+ overwrite_string = "lloWorl"
+ s = 2
+ */
+/* 출력 */
+// "HelloWorld"
+
+import Foundation
