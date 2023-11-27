876c135bae327512afff36bfdbf8ed3fc97dea5c
Eunice <jes.e.0927@gmail.com>
2023-11-27 09:37:11 +0900

    🎉 Init: Start new quiz(Programmers Lv.0 181942 문자열 섞기)

---
+++ CodingTestStudy/Programmers/Lv0/181942.swift
@@ -0,0 +1,18 @@
+//
+//  181942.swift
+//  CodingTestStudy
+//
+//  Created by Eunsu JEONG on 11/27/23.
+//
+/* 제목 */
+// 문자열 섞기
+/* 문제 */
+// 길이가 같은 두 문자열 str1과 str2가 주어집니다.
+// 두 문자열의 각 문자가 앞에서부터 서로 번갈아가면서 한 번씩 등장하는 문자열을 만들어 return 하는 solution 함수를 완성해 주세요.
+/* 입력 */
+// "aaaaa"
+// "bbbbb"
+/* 출력 */
+// "ababababab"
+
+import Foundation
