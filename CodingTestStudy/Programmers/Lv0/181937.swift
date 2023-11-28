fb5f5ad81bf2e1183ba982807cb257674235db52
Eunice <jes.e.0927@gmail.com>
2023-11-28 19:45:16 +0900

    🎉 Init: Start new quiz(Programmers Lv.0 181937 n의 배수)

--- CodingTestStudy/Programmers/Lv0/181937.swift
+++ CodingTestStudy/Programmers/Lv0/181937.swift
@@ -4,5 +4,20 @@
 //
 //  Created by Eunsu JEONG on 11/28/23.
 //
+/* 제목 */
+// n의 배수
+/* 문제 */
+// 정수 num과 n이 매개 변수로 주어질 때, num이 n의 배수이면 1을 return n의 배수가 아니라면 0을 return하도록 solution 함수를 완성해주세요.
+/* 예제 */
+/*
+ 98
+ 2
+ -> 1
+ */
+/*
+ 34
+ 3
+ -> 0
+ */
 
 import Foundation
